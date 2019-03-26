818ce39d9dcbe3f7d59f2d53bf23a307aae86d3e
Aaron Magid <ahm64@case.edu>
2019-03-26 01:26:21 -0400

    feat(Build): Re-added all numbers project content.

--- src/test/java/numbers/InputTest.java
+++ src/test/java/numbers/InputTest.java
@@ -1,30 +1,39 @@
 package numbers;
 
-import org.junit.Assert;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import numbers.DecimalInput.TestHook;
+
 /** Some example tests of parser **/
 public class InputTest {
+
+	// For using hook methods that are not object-specific
+	private static final TestHook hook = new DecimalInput("").new TestHook();
 
 	/** hasValidMiddlePadding tests **/
 	/* Example: 1_234 -> valid */
 	@Test
 	public void test_padding_nominal() {
-		Assert.assertTrue(true);
+		assertTrue(hook.hasValidMiddlePadding("1_234"));
 	}
 
-	/** hasValidMiddlePadding tests **/
-	/* Example: 1_234 -> valid */
+	/* Example: 1__234 -> valid */
 	@Test
-	public void test_padding_nominal2() {
-		Assert.assertTrue(true);
+	public void test_padding_long_underscore() {
+		assertTrue(hook.hasValidMiddlePadding("1__234"));
 	}
 
-	/** hasValidMiddlePadding tests **/
-	/* Example: 1_234 -> valid */
+	/* Example: 12_34 -> invalid */
 	@Test
-	public void test_padding_nominal3() {
-		Assert.assertTrue(true);
+	public void test_padding_bad_underscore() {
+		assertFalse(hook.hasValidMiddlePadding("12_34"));
+	}
+
+	/* Example: _1_234 -> invalid */
+	@Test
+	public void test_padding_leading_underscore() {
+		assertFalse(hook.hasValidMiddlePadding("_1_234"));
 	}
 }
