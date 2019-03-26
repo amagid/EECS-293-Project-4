c3b6534a7b6c51017184ab7a9165171ea3d7c077
Aaron Magid <ahm64@case.edu>
2019-03-26 02:40:41 -0400

    test(Input): Updated simulate_input test to use a valid input.

--- src/test/java/numbers/InputTest.java
+++ src/test/java/numbers/InputTest.java
@@ -45,7 +45,7 @@ public class InputTest {
 	/* Example: _1_234 -> invalid */
 	@Test
 	public void test_simulated_input() {
-		String result = floatingPointDriverHook.simulateInput("");
+		String result = floatingPointDriverHook.simulateInput("12.3456e2");
 		System.out.println("============" + result);
 		assertTrue(true);
 	}
