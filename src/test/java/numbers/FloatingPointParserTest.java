70105901994b9e2986469f09a296ef6b634060ba
Aaron Magid <ahm64@case.edu>
2019-03-31 21:26:16 -0400

    test(FloatingPointParser): Added test for the default assigned exponent.

--- src/test/java/numbers/FloatingPointParserTest.java
+++ src/test/java/numbers/FloatingPointParserTest.java
@@ -13,5 +13,13 @@ public class FloatingPointParserTest {
 	// For using hook methods that are not object-specific
 	private static final FloatingPointParserTestHook floatingPointParserHook = new FloatingPointParser.FloatingPointParserTestHook();
 
+    /** default exponent tests **/
     
+    @Test
+    public void test_constructor_empty_input_assigns_zero_exponent() {
+        DecimalInput generatedExpo = floatingPointParserHook.getDefaultGeneratedExponent();
+        DecimalInput expectedExpo = new DecimalInput("0");
+        
+        assertTrue(generatedExpo.toString() == expectedExpo.toString());
+    }
 }
