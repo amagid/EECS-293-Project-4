bfb565b3b8d249e8332af358cb7161c14821ff8d
Aaron Magid <ahm64@case.edu>
2019-04-01 22:15:32 -0400

    test(FloatingPointDriver): Added tests for getFloatingPointParser.

--- src/test/java/numbers/FloatingPointDriverTest.java
+++ src/test/java/numbers/FloatingPointDriverTest.java
@@ -5,6 +5,10 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import numbers.FloatingPointDriver.FloatingPointDriverTestHook;
+import java.util.Optional;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.ByteArrayInputStream;
 
 
 /** Some example tests of parser **/
@@ -13,4 +17,23 @@ public class FloatingPointDriverTest {
 	// For using hook methods that are not object-specific
 	private static final FloatingPointDriverTestHook floatingPointDriverHook = new FloatingPointDriver.FloatingPointDriverTestHook();
 
+	@Test
+	public void test_get_floating_point_parser_valid_input() {
+		String input = "1.0";
+		FloatingPointParser result = floatingPointDriverHook.getFloatingPointParser(input);
+
+		assertTrue(result.isValidInput());
+	}
+
+	@Test(expected = NumberFormatException.class)
+	public void test_get_floating_point_parser_internal_whitespace() {
+		String input = "1 . 0";
+		FloatingPointParser result = floatingPointDriverHook.getFloatingPointParser(input);
+	}
+
+	@Test(expected = NumberFormatException.class)
+	public void test_get_floating_point_parser_only_whitespace() {
+		String input = "    ";
+		FloatingPointParser result = floatingPointDriverHook.getFloatingPointParser(input);
+	}
 }
